1ef72f2ab39e8ddb545e7f5309cee0accbd5f861
YannickLongval <yannicklongval2014@gmail.com>
2023-06-21 19:59:15 -0400

    convert edge list to adjacency list, and working with undirected paths

--- graph_theory/searching.py
+++ graph_theory/searching.py
@@ -1,5 +1,7 @@
 # Searching algorithms
 
+# One representation of a graph: adjacency list
+# Keys are the nodes, values are the edges
 graph:dict[str, list[str]] = {
     "A": ["B", "C"],
     "B": ["D"],
@@ -8,6 +10,42 @@ graph:dict[str, list[str]] = {
     "E": [],
     "F": []
 }
+
+# Another respresentation of a graph: edge list
+# Each tuple is an edge between two nodes
+edges:list[tuple[str,str]] = [
+    ("I", "J"),
+    ("K", "I"),
+    ("M", "K"),
+    ("K", "L"),
+    ("O", "N"),
+]
+
+# Convert an edge list to an adjacency list
+def edgeToAdjacency(edges:list[tuple[str,str]])->dict[str, list[str]]:
+    graph = {}
+    for a, b in edges:
+        graph.setdefault(a, []).append(b)
+        graph.setdefault(b, []).append(a)
+    return graph
+
+def hasPathUndirected(graph:dict[str, list[str]], src:str, dst:str, visited:set[str])->bool:
+    if src == dst: return True
+    if src in visited: return False
+
+    visited.add(src)
+
+    for neighbor in graph[src]:
+        if hasPathUndirected(graph, neighbor, dst, visited):
+            return True
+        
+    return False
+
+def undirectedPath(edges:list[tuple[str,str]], nodeA:str, nodeB:str)->bool:
+    graph = edgeToAdjacency(edges)
+    return hasPathUndirected(graph, nodeA, nodeB, set())
+
+print(undirectedPath(edges, "I", "O"))
 
 # An iterative implementation of Depth First Search
 def DFSIter(graph:dict[str, list[str]], start:str):
@@ -40,12 +78,26 @@ def BFSIter(graph:dict[str, list[str]], start:str):
 
 # Algorithm to determine whether or not a path can be found between 2 points 
 # Using a DFS
-def hasPath(graph:dict[str, list[str]], src:str, dst:str):
+def hasPathDFS(graph:dict[str, list[str]], src:str, dst:str)->bool:
     if src == dst: return True
 
     for neighbor in graph[src]:
-        if hasPath(graph, neighbor, dst): return True
+        if hasPathDFS(graph, neighbor, dst): return True
 
     return False #Only return false once all of the paths have been checked
 
-BFSIter(graph, "A")
+# Algorithm to determine whether or not a path can be found between 2 points 
+# Using a BFS
+def hasPathBFS(graph:dict[str, list[str]], src:str, dst:str)->bool:
+    # Array implementation of a Queue (beginning of queue is at start of array)
+    queue:list[str] = [src]
+
+    while len(queue) > 0:
+        curr = queue.pop(0)
+        if curr == dst: return True
+        for neighbor in graph[curr]:
+            queue.append(neighbor)
+
+    return False #Only return false once all of the paths have been checked
+
+print(hasPathBFS(graph, "A", "F"))
